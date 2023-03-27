26d6250fa3e12c4482b3628bfa4efbd707c39ed6
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-27 15:34:05 +0300

    Create modals for adding todo and project

--- src/domSelectors.js
+++ src/domSelectors.js
@@ -1,12 +1,24 @@
+// navbar
+const plusTodo = document.querySelector(".task-modal-opener");
+
+// sidebar
+const plusProject = document.querySelector(".projects");
+
 // add todo modal
+const todoModal = document.querySelector(".todo-modal");
 const newTitle = document.querySelector("#title").value;
 const newDescription = document.querySelector("#description").value;
 const newDueDate = document.querySelector("#due-date").value;
-const newProject = document.querySelector("#project").value;
+const newProject = document.querySelector("#projects").value;
 const newImportant = document.querySelector("#important");
+const addTodoButton = document.querySelector(".add-todo");
+const cancelTodoButton = document.querySelector(".cancel-todo");
 
 // add project modal
-const newProjectName = document.querySelector("#name").value;
+const projectModal = document.querySelector(".project-modal");
+const newProjectName = document.querySelector("#name");
+const addProjectButton = document.querySelector(".add-project");
+const cancelProjectButton = document.querySelector(".cancel-project");
 
 export {
   newTitle,
@@ -15,4 +27,12 @@ export {
   newProject,
   newImportant,
   newProjectName,
+  todoModal,
+  addProjectButton,
+  addTodoButton,
+  cancelProjectButton,
+  cancelTodoButton,
+  projectModal,
+  plusTodo,
+  plusProject,
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import {
+  cancelProjectButton,
+  cancelTodoButton,
+  plusProject,
+  plusTodo,
+  projectModal,
+  todoModal,
+} from "./domSelectors";
+
+plusTodo.addEventListener("click", () => {
+  todoModal.style.display = "block";
+});
+
+cancelTodoButton.addEventListener("click", () => {
+  todoModal.style.display = "none";
+});
+
+plusProject.addEventListener("click", () => {
+  projectModal.style.display = "block";
+});
+
+cancelProjectButton.addEventListener("click", () => {
+  projectModal.style.display = "none";
+});
